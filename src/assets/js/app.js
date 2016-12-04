2966bb9c37bf6b8cee57997bd1730af744c5f2df
aneesha bharadwaj <aneesha@aneeshas-MacBook-Pro.local>
2016-12-04 15:41:31 -0800

    Adding all images, html, css and js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,63 @@
 $(document).foundation();
+
+
+/* LOG IN */
+
+$(document).ready(function(){
+  $('#login-trigger').click(function(){
+    $(this).next('#login-content').slideToggle();
+    $(this).toggleClass('active');
+
+    if ($(this).hasClass('active')) $(this).find('span').html('&#x25B2;')
+      else $(this).find('span').html('&#x25BC;')
+    })
+});
+
+
+
+/* HELP */
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function loginFunction() {
+    document.getElementById("LoginDropdown").classList.toggle("show");
+}
+
+
+
+/* SLIDESHOW */
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+
+
+
+
